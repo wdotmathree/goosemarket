17b76ce2c0c9158b86af18dc6c9303df06d82d47
William Ma <w74ma@uwaterloo.ca>
2025-10-09 14:03:27 -0400

    Refactor test dependencies into general file

--- To-Do-App/tests/test_add.py
+++ To-Do-App/tests/test_add.py
@@ -1,20 +1,6 @@
-from datetime import datetime, timedelta
-import pytest
+from common import *
 
-from src.main import add, get_connection
-
-def get_all_tasks():
-	# Helper to fetch all tasks from DB for assertions
-	with get_connection() as conn:
-		with conn.cursor() as cursor:
-			cursor.execute("SELECT userid, name, type, started, due, done FROM todo")
-			return [row for row in cursor.fetchall()]
-
-def clear():
-	with get_connection() as conn:
-		with conn.cursor() as cursor:
-			cursor.execute("DELETE FROM todo")
-			conn.commit()
+from src.main import add
 
 def test_add_task():
 	clear()
